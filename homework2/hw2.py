dfbf7a054f4b362917cc4dae4c722cb21a69f785
Elizabeth Hung <elizabethhung@wifi-10-42-159-121.wifi.berkeley.edu>
2025-02-09 00:12:58 -0800

    Adding finished homeowrk 2' git stauts git status

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,27 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+#       cd ~/Desktop
 # 2) What does ~/ mean?
+#       The ~/ means that you are in your home directory.
 # 3) What's the difference between and abolute and a relative path in your terminal?
+#       The absolute path lets you navigate directly to the file or directory that you specify. Relative paths, as the name suggest, are relative to the current working directory and often require symbols like .. to navigate between directories.
 # 4) What command would bring you back to your home directory?
+#       cd 
 # 5) If you called rm ./ in your current working directory, what would it do? 
+#       It would remove all files and directories from the current working directory (specified with the ./)
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+#       Git add puts a file or directory in the staging area. Git commit is used to save changes to the local repository and requires a message. Git push published it to your remote repository (GitHub).
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+#       Git add requires a file or directory to be specified. For new files:
+#           On branch main
+#           Changes to be committed:
+#       and then it displays what changes will be made such as "new file:   hi.txt"
+#       For files that have already been pushed to GitHub with no changes, the terminal will display 
+#           On branch main
+#           nothing to commit, working tree clean
+#       This tells you what changes there are to be made (if any).
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+#       We haven't done anything hard enough to warrant frustrating errors or bugs.
 # 9) How does cloning a repository differ from pulling from a repository?
+#       Cloning a repository copies a remote repository to the local repository and will include all files. Pulling will download the latest content from the remote repository, sort of like installing an update.
 # 10) Tell me a fun fact!
+#       If you travel at a steady 65 mph, it would take you 4.8x10^17 years to reach the edge of the observable universe. (If you wanted a fun fact about me: I'm a theatre kid.)
